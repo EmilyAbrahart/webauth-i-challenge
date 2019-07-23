b8f3dfd0dedf93d7c0de875d89c57483434dca55
EmilyAbrahart <emilyabrahart@gmail.com>
2019-07-23 14:11:11 +0100

    Rearrange directory and split routes into separation directories

---
+++ api/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
+const authRouter = require('../auth/auth-router.js');
+const usersRouter = require('../users/users-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(session({
+  name: 'sessionId', 
+  secret: 'keep it secret, keep it long', // we intend to encrypt
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new KnexSessionStore({
+    knex: require('../data/db.js'), 
+    tablename: 'sessions', 
+    sidfieldname: 'sessionid', 
+    createtable: true, 
+    clearInterval: 1000 * 60 * 60, 
+  }),
+}));
+server.use(cors());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', usersRouter);
+
+server.get('/', (req, res) => {
+	res.send('Welcome to WebAuth I Challenge');
+});
+
+module.exports = server;
